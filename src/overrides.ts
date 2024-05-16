50de8d1cb83a9219840af7b1e32853980805a7d9
Dmitriy <kagno.dmitriy@gmail.com>
2024-05-15 23:15:19 -0400

    Add Status Effect overrides for easier testing

--- src/overrides.ts
+++ src/overrides.ts
@@ -11,6 +11,7 @@ import { Type } from './data/type';
 import { Stat } from './data/pokemon-stat';
 import { PokeballCounts } from './battle-scene';
 import { PokeballType } from './data/pokeball';
+import { StatusEffect } from './data/status-effect';
 
 /**
  * Overrides for testing different in game situations
@@ -51,6 +52,7 @@ export const STARTING_LEVEL_OVERRIDE: integer = 0;
 export const ABILITY_OVERRIDE: Abilities = Abilities.NONE;
 export const PASSIVE_ABILITY_OVERRIDE: Abilities = Abilities.NONE;
 export const MOVESET_OVERRIDE: Array<Moves> = [];
+export const STATUS_OVERRIDE: StatusEffect = StatusEffect.NONE;
 export const SHINY_OVERRIDE: boolean = false;
 export const VARIANT_OVERRIDE: Variant = 0;
 
@@ -61,6 +63,7 @@ export const VARIANT_OVERRIDE: Variant = 0;
 export const OPP_ABILITY_OVERRIDE: Abilities = Abilities.NONE;
 export const OPP_PASSIVE_ABILITY_OVERRIDE = Abilities.NONE;
 export const OPP_MOVESET_OVERRIDE: Array<Moves> = [];
+export const OPP_STATUS_OVERRIDE: StatusEffect = StatusEffect.NONE;
 export const OPP_SHINY_OVERRIDE: boolean = false;
 export const OPP_VARIANT_OVERRIDE: Variant = 0;
 
